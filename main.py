368bc87daece563bbd026d350831f4e638fa62bb
Ethan Page <ethanriverpage@gmail.com>
2022-11-13 22:06:36 -0500

    Main code written

--- main.py
+++ main.py
@@ -18,5 +18,40 @@ def pythagorea():
     c = math.sqrt(a ** 2 + b ** 2)
     print(f"The length of the hypotenuse c is {c}")
 
+def fibonacci():
+    power = int(input("Fibonacci to what power?"))
+    print(fib(power))
+
+def systest():
+    print("What is your name?")
+    name = sys.stdin.readline()
+    print("What is your age?")
+    age = sys.stdin.readline()
+    print("...")
+    print("...")
+    print("Name: " + name + "Age: " + age)
+
+def whiletest():
+    while True:
+        whiletest_input = int(input("Pick a number. "))
+        if whiletest_input >= -1:
+            whiletest_input = whiletest_input - 5
+            print(whiletest_input)
+        elif whiletest_input < -1:
+            print(str(whiletest_input) + " is the final number. Goodbye.")
+            break
+        else:
+            return 1
+
+print("sys input test")
+systest()
+print("Fibonacci test")
+fibonacci()
+print("Foobar test")
 foobar()
+print("Pythagorean theorum test")
 pythagorea()
+print("while test")
+whiletest()
+
+print("This is the end.")
